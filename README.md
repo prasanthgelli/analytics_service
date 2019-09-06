424259834b7214a1e2abeef99a65a6d3a6a0a2a4
Prasanth <Gelliprasanth27@gmail.com>
2019-09-06 16:14:27 +0530

    Add files via upload
    Added required files

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# analytics_service
+# Empty Python Package
+
+This package is empty. 
+
+To install this package: 
+
+```
+pip install git+git://github.com/madendorff/functions --upgrade
+```
+
+Prereqs are:
+
+iotfunctions. See https://github.com/ibm-watson-iot/functions
+
---
+++ custom/multiplybyfactor.py
@@ -0,0 +1,39 @@
+import inspect
+import logging
+import datetime as dt
+import math
+from sqlalchemy.sql.sqltypes import TIMESTAMP,VARCHAR
+import numpy as np
+import pandas as pd
+
+from iotfunctions.base import BaseTransformer
+from iotfunctions import ui
+
+
+PACKAGE_URL = 'git+https://github.com/prasanthgelli/analytics_service/functions@'
+
+class MultiplyByFactor(BaseTransformer):
+
+    def __init__(self, input_items,output_items):
+
+        self.input_items = input_items
+        self.output_items = output_items
+        super().__init__()
+    def execute(self, df):
+        df = df.copy()
+        for i,input_item in enumerate(self.input_items):
+            df[self.output_items[i]] = df[input_item] * 2
+        return df
+    @classmethod
+    def build_ui(cls):
+        #define arguments that behave as function inputs
+        inputs = []
+        inputs.append(ui.UIMultiItem(
+                name = 'input_items',
+                datatype=float,
+                description = "Data items adjust",
+                output_item = 'output_items',
+                is_output_datatype_derived = True)
+                      )        
+        outputs = []
+        return (inputs,outputs)
