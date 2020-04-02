e5a543ca295654cf292791e8812084ea5398b3f8
pragelli <pragelli@in.ibm.com>
2020-04-02 17:52:16 +0530

    Modified the aggregation

--- custom/multiplybyfactor.py
+++ custom/multiplybyfactor.py
@@ -12,17 +12,17 @@ from iotfunctions import ui
 
 PACKAGE_URL = 'git+https://github.com/prasanthgelli/analytics_service@master'
 
-class MultiplyByFactor(BaseTransformer):
+class meangp(BaseTransformer):
 
     def __init__(self, input_items,output_items):
-
+        print("Here")
         self.input_items = input_items
         self.output_items = output_items
         super().__init__()
     def execute(self, df):
         df = df.copy()
         for i,input_item in enumerate(self.input_items):
-            df[self.output_items[i]] = df[input_item] * 2
+            df[self.output_items[i]] = df[input_item] * 3
         return df
     @classmethod
     def build_ui(cls):
--- scripts/local_test_of_function.py
+++ scripts/local_test_of_function.py
@@ -2,52 +2,22 @@ import datetime as dt
 import json
 import pandas as pd
 import numpy as np
-import logging
 from sqlalchemy import Column, Integer, String, Float, DateTime, Boolean, func
+from iotfunctions.base import BaseTransformer
+from iotfunctions.metadata import EntityType
 from iotfunctions.db import Database
-from iotfunctions.enginelog import EngineLogging
+from iotfunctions import ui
 
-EngineLogging.configure_console_logging(logging.DEBUG)
-
-'''
-You can test functions locally before registering them on the server to
-understand how they work.
-
-Supply credentials by pasting them from the usage section into the UI.
-Place your credentials in a separate file that you don't check into the repo. 
-
-'''
-
-with open('credentials_as_dev.json', encoding='utf-8') as F:
-    credentials = json.loads(F.read())
+with open('credentials_as.json', encoding='utf-8') as F:
+  credentials = json.loads(F.read())
 db_schema = None
 db = Database(credentials=credentials)
 
-'''
-Import and instantiate the functions to be tested 
-
-The local test will generate data instead of using server data.
-By default it will assume that the input data items are numeric.
-
-Required data items will be inferred from the function inputs.
-
-The function below executes an expression involving a column called x1
-The local test function will generate data dataframe containing the column x1
-
-By default test results are written to a file named df_test_entity_for_<function_name>
-This file will be written to the working directory.
-
-'''
-
-from custom.functions import HelloWorld
-fn = HelloWorld(
-        name = 'Mike',
-        greeting_col = 'greeting')
-fn.execute_local_test(db=db,db_schema=db_schema)
-
-'''
-Register function so that you can see it in the UI
-'''
-
-db.register_functions([HelloWorld])
-
+from custom.multiplybyfactor import MultiplyByFactor
+fn = MultiplyByFactor(
+    input_items = ['orgoccupancycount', 'occupancycount'],
+    factor = 2,
+    output_items = ['adjusted_orgoccupancycount', 'adjusted_occupancycount']
+              )
+df = fn.execute_local_test(db=db, db_schema=db_schema, generate_days=1,to_csv=True)
+print(df)
